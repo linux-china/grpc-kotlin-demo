98c75484e5ad3e8a99560d6229e48eb7835035c3
linux_china <libing.chen@gmail.com>
2020-04-16 13:31:13 -0700

    GreeterService

---
+++ src/main/kotlin/org/mvnsearch/greeter/GreeterService.kt
@@ -0,0 +1,35 @@
+package org.mvnsearch.greeter
+
+import io.grpc.ManagedChannel
+import kotlinx.coroutines.flow.Flow
+import org.mvnsearch.grpc.GreeterServiceGrpcKt.GreeterServiceCoroutineStub
+import org.mvnsearch.grpc.HelloReply
+import org.mvnsearch.grpc.HelloRequest
+
+/**
+ * Greeter Coroutines Service
+ *
+ * @author linux_china
+ */
+interface GreeterService {
+  suspend fun sayHello(request: HelloRequest): HelloReply
+  fun sayHellos(request: HelloRequest): Flow<HelloReply>
+}
+
+/**
+ * Greeter Service Stub to call remote gRPC service
+ * @param channel managed channel
+ */
+class GreeterServiceStub constructor(
+  channel: ManagedChannel
+) : GreeterService {
+  private val stub: GreeterServiceCoroutineStub = GreeterServiceCoroutineStub(channel)
+
+  override suspend fun sayHello(request: HelloRequest): HelloReply {
+    return stub.sayHello(request)
+  }
+
+  override fun sayHellos(request: HelloRequest): Flow<HelloReply> {
+    return stub.sayHellos(request)
+  }
+}
